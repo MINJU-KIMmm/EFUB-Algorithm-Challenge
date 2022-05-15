68cad4a2fac0e10753993b4cb7d1bb2b3470ea04
sohyundoh <lyny123@naver.com>
2022-05-15 10:46:25 +0900

    [BOJ] 블랙잭 / 브론즈2 /https://www.acmicpc.net/problem/2798

--- sohyundoh/[BOJ]2798.cpp
+++ sohyundoh/[BOJ]2798.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int card[300]; // 카드값을 입력받을 배열
+int card[300];
 int main() {
 	int n, tn;
 	cin >> n >> tn;
@@ -12,11 +12,11 @@ int main() {
 	for (int j = 0; j < n; j++) {
 		for (int k = 0; k < n; k++) {
 			for (int i = 0; i < n; i++) {
-				if (i == j || j == k || i == k) { //같은 카드를 뽑을 수는 없음
+				if (i == j || j == k || i == k) { 
 					continue;
 				}
-				int sum = card[i] + card[j] + card[k]; //카드 값을 일일이 다 계산 시간복잡도: O(n^3)
-				if (sum > max && sum <= tn) { //입력받은 값보다 작은데 가장 큰 값 찾기
+				int sum = card[i] + card[j] + card[k]; 
+				if (sum > max && sum <= tn) {
 					max = sum;
 					break;
 				}
