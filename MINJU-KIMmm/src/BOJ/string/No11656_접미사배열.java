cf546f3a869f8b948ef1b8b8a799cacb55f1241a
MINJU-KIMmm <0112km@ewhain.net>
2022-05-21 17:08:30 +0900

    [BOJ] 접미사배열 / 실버4 / https://acmicpc.net/problem/11656

--- MINJU-KIMmm/src/BOJ/string/No11656_접미사배열.java
+++ MINJU-KIMmm/src/BOJ/string/No11656_접미사배열.java
@@ -7,17 +7,12 @@ public class No11656_접미사배열 {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         String S=br.readLine();
 
-        HashSet<String> set=new HashSet<>();
+        ArrayList<String> arr=new ArrayList<>();
 
-        StringBuilder sb=new StringBuilder();
-        for(int i=S.length()-1;i>=0;i--){
-            sb.append(S.charAt(i));
-            sb.reverse();
-            set.add(sb.toString());
-            sb.reverse();
+        for(int i=0;i<S.length();i++){
+            arr.add(S.substring(i));
         }
 
-        ArrayList<String> arr=new ArrayList<>(set);
         Collections.sort(arr);
 
         for(int i=0;i<arr.size();i++){
