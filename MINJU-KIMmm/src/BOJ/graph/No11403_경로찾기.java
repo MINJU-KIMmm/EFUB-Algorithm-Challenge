dde0205b247c5067d949ec99de6693316ea32659
MINJU-KIMmm <0112km@ewhain.net>
2022-06-20 22:12:16 +0900

    [BOJ] 경로찾기 / 실버1 / https://acpicmc.net/problem/11403
    https://red-mimmu.tistory.com/83

---
+++ MINJU-KIMmm/src/BOJ/graph/No11403_경로찾기.java
@@ -0,0 +1,33 @@
+package BOJ.graph;
+
+import java.util.*;
+import java.io.*;
+public class No11403_경로찾기 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int N=Integer.parseInt(br.readLine());
+
+        int[][] arr=new int[N][N];
+        for(int i=0;i<N;i++){
+            StringTokenizer st=new StringTokenizer(br.readLine());
+            for(int j=0;j<N;j++){
+                arr[i][j]=Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int k=0;k<N;k++){
+            for(int i=0;i<N;i++){
+                for(int j=0;j<N;j++){
+                    if(arr[i][k]==1&&arr[k][j]==1) arr[i][j]=1;
+                }
+            }
+        }
+
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
