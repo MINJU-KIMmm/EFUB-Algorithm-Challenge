94ba396d85c74cd5f158ec59270176930a1643b2
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2022-06-26 23:14:55 +0900

    [BOJ] 시험 감독 / 브론즈2 / https://www.acmicpc.net/problem/13458
    https://dev-minji.tistory.com/159

---
+++ nitronium102/ETC/정렬/시험 감독.cpp
@@ -0,0 +1,39 @@
+﻿//
+// Created by KangMinji on 2022-06-26.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, b, c;
+    long long num = 0;
+    cin >> n;
+    vector<int> student(n);
+
+    // 입력
+    for (int i=0; i<n; i++)
+        cin >> student[i];
+    cin >> b >> c;
+
+    // 연산
+    for (int i=0; i<n; i++){
+        int cnt = student[i];
+        // 총감독관
+        cnt -= b;
+        num++;
+
+        // 총감독관만으로도 충분한 경우
+        if (cnt <= 0)
+            continue;
+
+        // 부감독관
+        num += (cnt / c);
+        if (cnt % c != 0)
+            num++;
+    }
+
+    // 출력
+    cout << num;
+}
