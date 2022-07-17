2f4fb330d238393f5d8324fb278b8034191e6a7e
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2022-07-17 22:38:30 +0900

    [BOJ] 서로 다른 부분 문자열의 개수 / 실버3 / https://www.acmicpc.net/problem/11478
    https://dev-minji.tistory.com/171

---
+++ nitronium102/BOJ/맵과셋/서로 다른 부분 문자열의 개수.cpp
@@ -0,0 +1,31 @@
+﻿//
+// Created by KangMinji on 2022-07-17.
+//
+
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    string s;
+
+    cin >> s;
+
+    set<string> subStr;
+    /* 1) [0] [1] [2] [3] [4]
+     * 2) [0-1] [1-2] [2-3] [3-4]
+     * 3) [0-1-2] [1-2-3] [2-3-4]
+     * 4) [0-1-2-3] [1-2-3-4]
+     * 5) [0-1-2-3-4]
+     * => 0으로 시작하는 것 / 1로 시작하는 것 / 2로 시작하는 것 / ...
+     */
+    for (int i=0; i<s.length(); i++){
+        string tmp = "";
+        for (int j=i; j<s.length(); j++){
+            tmp += s[j];
+            subStr.insert(tmp);
+        }
+    }
+
+    cout << subStr.size();
+}
