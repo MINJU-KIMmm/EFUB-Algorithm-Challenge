9c07d8692ca5e0f015af22362cb52efc7a7057ec
sohyundoh <lyny123@naver.com>
2022-08-14 19:58:37 +0900

    [BOJ] 다리놓기/ 실버 5/https://www.acmicpc.net/problem/1010
    https://www.notion.so/BOJ-1c05bc6147b4400bba7898389478433f

---
+++ sohyundoh/[BOJ]/[BOJ]1010.cpp
@@ -0,0 +1,27 @@
+﻿﻿
+#include <stdio.h>
+
+long long rs_nCr(int n, int r)
+{
+    int i, j, t = n - r;
+    long long res = 1.;
+
+    if (t < r) t = n - (r = n - r);
+    for (i = t + 1; i <= n; ++i) res *= i;
+    for (i = 2; i <= r; ++i) res /= i;
+    return res;
+}
+
+int main()
+{
+    int T, N, R;
+
+    scanf("%d", &T);
+    for (int i = 0; i < T; i++)
+    {
+        scanf("%d %d", &R, &N);
+        printf("%lld\n", rs_nCr(N, R));
+    }
+    return 0;
+}
+﻿
