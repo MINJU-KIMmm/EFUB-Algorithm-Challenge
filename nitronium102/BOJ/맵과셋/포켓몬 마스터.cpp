68cf80c366913a6630199a63722dcae1973dd680
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2022-09-11 23:11:41 +0900

    [BOJ] 나는야 포켓몬 마스터 이다솜 / 실버4 / https://www.acmicpc.net/problem/1620

---
+++ nitronium102/BOJ/맵과셋/포켓몬 마스터.cpp
@@ -0,0 +1,35 @@
+﻿//
+// Created by KangMinji on 2022-09-07.
+//
+
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, m;
+    string input;
+    map<string, int> pocket_name;
+    map<int, string> pocket_num;
+
+    // 입력
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        cin >> input;
+        pocket_name[input] = i;
+        pocket_num[i] = input;
+    }
+
+    // 출력
+    while (m--) {
+        cin >> input;
+        if (isdigit(input[0]))
+            cout << pocket_num[stoi(input)] << "\n";
+        else
+            cout << pocket_name[input] << "\n";
+    }
+}
