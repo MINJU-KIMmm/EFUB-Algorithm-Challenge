72798be0dfb338bbc353ae19d33c0350bd4373b0
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2022-09-25 21:50:53 +0900

    [BOJ] 백대열 / 실버5 / https://www.acmicpc.net/problem/14490

---
+++ nitronium102/BOJ/정수론/백대열.cpp
@@ -0,0 +1,29 @@
+﻿//
+// Created by KangMinji on 2022-09-19.
+//
+
+#include <iostream>
+
+using namespace std;
+typedef long long ll;
+
+ll findGCD(ll x, ll y) {
+    if (y == 0)
+        return x;
+    return findGCD(y, x % y);
+}
+
+int main() {
+    ll x, y;
+
+    // 입력
+    cin >> x >> y;
+
+    // 연산
+    ll gcd = findGCD(x, y);
+    if (gcd > 1) { // 교차점을 지남
+        cout << x + y - gcd << "\n";
+    } else {
+        cout << x + y - 1 << "\n";
+    }
+}
