9cac33d6ae47f9ea0c601c58ef26f0301c7d0734
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2022-10-09 23:21:13 +0900

    [BOJ] N번째 큰 수 / 실버2 / https://www.acmicpc.net/problem/2075

---
+++ nitronium102/BOJ/우선순위큐/2075.cpp
@@ -0,0 +1,33 @@
+﻿//
+// Created by KangMinji on 2022-10-05.
+//
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    int n, x;
+
+    // 입출력 시간
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // 입력
+    cin >> n;
+    priority_queue<int, vector<int>, greater<>> pq; // 최소힙
+
+    for (int i=0; i<n*n; i++) {
+        cin >> x;
+
+        pq.push(x);
+
+        if (pq.size() > n) { // 최소 힙의 크기가 n을 넘어가면 가장 작은 값(root) 삭제
+            pq.pop();
+        }
+    }
+
+    // 3) root 출력
+    cout << pq.top() << "\n";
+}
