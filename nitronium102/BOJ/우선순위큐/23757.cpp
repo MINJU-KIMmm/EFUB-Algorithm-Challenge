18814bff90bbe0a3a7a57c7a652c7eccf1a73243
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2022-10-09 23:02:56 +0900

    [BOJ] 아이들과 선물 상자 / 실버2 / https://www.acmicpc.net/problem/23757

---
+++ nitronium102/BOJ/우선순위큐/23757.cpp
@@ -0,0 +1,49 @@
+﻿//
+// Created by KangMinji on 2022-10-05.
+//
+
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int checkPresentAvailable(priority_queue<int> &present,
+                          vector<int> &child){
+    for (int i=0; i<child.size(); i++) {
+        // 각자의 max를 비교
+        int item = present.top();
+        // 2) 선물 가져갈 수 없는 경우
+        if (item < child[i]){
+            return 0;
+        }
+
+        // 1) 선물 가져갈 수 있는 경우
+        // 각자의 max를 비교
+        // 해당 노드의 값 변경
+        present.pop();
+        present.push(item - child[i]);
+    }
+    return 1;
+}
+
+int main() {
+    int n, m, x;
+
+    // 입력
+    cin >> n >> m;
+    priority_queue<int> present;
+    vector<int> child(m, 0);
+
+    for (int i=0; i<n; i++){
+        cin >> x;
+        present.push(x);
+    }
+
+    // [!] 아이들은 순서대로 가져감 -> 내림차순 정렬하면 안 됨
+    for (int i=0; i<m; i++){
+        cin >> child[i];
+    }
+
+    // 연산
+    cout << checkPresentAvailable(present, child);
+}
