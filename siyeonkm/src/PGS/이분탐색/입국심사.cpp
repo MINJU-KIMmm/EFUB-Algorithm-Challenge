80d711d5ea51b4a29f80f6d8472b944ebe04f319
siyeonkm <68330823+siyeonkm@users.noreply.github.com>
2022-11-14 00:57:30 +0900

    [PGS] 입국심사 / lv3 / https://school.programmers.co.kr/learn/courses/30/lessons/43238

---
+++ siyeonkm/src/PGS/이분탐색/입국심사.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <climits> // long long의 최댓값을 이용합니다.
+
+using namespace std;
+
+long long solution(int n, vector<int> times) {
+	sort(times.begin(), times.end());
+
+        long long left = (long long)times[0];
+        long long right = (long long)times[times.size() - 1] * n;
+        long long answer = right;
+        while(left <= right){
+            long long mid = (right + left) / 2;
+            long long pass = 0;
+
+            for(int i = 0; i < times.size(); ++i)
+                pass += mid / (long long)times[i];
+
+            if(pass >= n){
+                right = mid - 1;
+                if(mid <= answer)
+                    answer = mid;
+            }
+            else
+                left = mid + 1;
+        }
+        return answer;
+}
