a929ede0ad3873005bd106358684fbdb6fc06dac
MINJU-KIMmm <0112km@ewhain.net>
2023-01-04 15:00:02 +0900

    [BOJ] 수영장만들기 / 골드1 / https://acmicpc.net/problem/1113
    복습

--- MINJU-KIMmm/src/study/week6/No3048_개미.py
+++ MINJU-KIMmm/src/study/week6/No3048_개미.py
@@ -14,9 +14,9 @@ while T:
     T -= 1
     index = []
     for i in range(len(arr)-1):
-        if arr[i][1]==0 and arr[i+1][1]==1:
-            index.append(i)
+        if arr[i][1]==0 and arr[i+1][1]==1: #group1 개미들을 기준으로
+            index.append(i) #여기서 그냥 바꾸면 바뀌었던 게 계속 다음 반복에서 바뀌기 때문에 index배열에 담은 다음에 한번에 바꾼다
     for i in index:
-        arr[i], arr[i+1] = arr[i+1], arr[i]
+        arr[i], arr[i+1] = arr[i+1], arr[i] #swap
 for i, j in arr:
     print(i, end='')
