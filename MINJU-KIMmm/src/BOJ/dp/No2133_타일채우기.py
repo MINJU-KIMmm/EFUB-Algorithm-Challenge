cdc824d9c06cbb69848e919b73d819177409c1e6
MINJU-KIMmm <0112km@ewhain.net>
2023-02-27 10:58:35 +0900

    [BOJ] 타일채우기(dp) / 골드4 / https://acmicpc.net/problem/2133

---
+++ MINJU-KIMmm/src/BOJ/dp/No2133_타일채우기.py
@@ -0,0 +1,14 @@
+N = int(input())
+
+dp = [0 for _ in range(N+1)]
+
+if N>=2:
+    dp[2] = 3
+
+for i in range(4, N+1, 2):
+    dp[i] += dp[i-2]*dp[2]
+    for j in range(4, i, 2):
+        dp[i] += dp[i-j]*2
+    dp[i] += 2
+
+print(dp[N])
